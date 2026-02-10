47d767d07f330de86f6b3924903f693aa9da064c
jeonjyu <jihoojeon77@gmail.com>
2026-02-10 22:05:36 +0900

    [feat] 아이템 데이터 관리 매니저 추가
    - 아이템 인터페이스
    - 아이템 항목별 데이터 관리 클래스
    - 아이템 오브젝트 관리 클래스

---
+++ Assets/0.Scripts/Store/StoreItem/StoreCategory.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+// 상점 항목별 아이템 관리하는 클래스
+public class StoreCategory<T> : IItem where T : StoreItemBase
+{
+    Dictionary<int, T> _category = new Dictionary<int, T>();
+
+    public Dictionary<int, T> Category => _category;
+    public int CategoryCount => _category.Count;
+
+    public void AddToCategory(T item)
+    {
+        _category.Add(item.ItemId, item);
+    }
+
+    public void RemoveFromCategory(T item) 
+    {
+        _category.Remove(item.ItemId);
+    }
+}
