654ccb90e3e91394123387081f64fe0eadf07d76
jeonjyu <jihoojeon77@gmail.com>
2026-02-11 12:00:44 +0900

    [feat] 상점 아이템 리스트 추가
    - 아이템 매니저 추가
    - 아이템 딕셔너리 리스트 추가

---
+++ Assets/0.Scripts/Store/StoreItem/ItemManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+public class ItemManager : Singleton<ItemManager>
+{
+    // 아이템 카탈로그 딕셔너리 모음
+    List<Dictionary<int, StoreItemBase>> _totalItemData = new List<Dictionary<int, StoreItemBase>>();
+
+    // 플레이어가 소유한 아이템 딕셔너리 모음
+    List<Dictionary<int, StoreItemBase>> _playerItemData = new List<Dictionary<int, StoreItemBase>>();
+}
