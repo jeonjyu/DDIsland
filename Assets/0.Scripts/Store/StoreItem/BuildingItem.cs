e608ae9e8737a1106cc5e0a3c75315588366cea9
jeonjyu <jihoojeon77@gmail.com>
2026-02-13 15:12:23 +0900

    [refactor] 아이템 데이터 저장 방식 SO로 변경

--- Assets/0.Scripts/Store/StoreItem/BuildingItem.cs
+++ Assets/0.Scripts/Store/StoreItem/BuildingItem.cs
@@ -8,8 +8,10 @@ enum InteriorType
 
 [Serializable]
 public class BuildingItem : StoreItemBase
-{
+{    
     [SerializeField] InteriorType _buildingType;
 
     internal InteriorType BuildingType => _buildingType;
+
+    // SO로 공통 정보 받아옴
 }
