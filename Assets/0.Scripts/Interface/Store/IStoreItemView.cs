233f619366c8364e61c1fe4359d0ca1113e22f3b
jeonjyu <jihoojeon77@gmail.com>
2026-02-13 17:47:11 +0900

    [feat] 상점 아이템 슬롯 뷰 추가

---
+++ Assets/0.Scripts/Interface/Store/IStoreItemView.cs
@@ -0,0 +1,6 @@
+﻿public interface IStoreItemView
+{
+    void Init(int price, string name, int count, bool isGained, string imgPath);
+    void UpdateItemCount(int count);
+    void UpdateSlotColor(bool isGained);
+}
