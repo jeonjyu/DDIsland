9a114a8ae6638980611b37ff00e2953b67ff65bc
jeonjyu <jihoojeon77@gmail.com>
2026-02-19 18:21:35 +0900

    [feat] 상점 드롭다운 불러오기 기능 추가

---
+++ Assets/0.Scripts/Store/StoreUI/DropdownOptions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel;
+
+// 드롭다운 옵션으로 설정해줄 enum들
+
+public enum InteriorFilter
+{
+    [Description("모든 아이템")] all,
+    [Description("메인 하우스")] mainHouse,
+    [Description("섬 바닥")] floor,
+    [Description("섬 고정 장식물")] fix,
+    [Description("섬 자유 배치물")] free,
+    [Description("호수 바닥")] lakeFloor,
+    [Description("호수 고정 장식물")] lakeFix,
+    [Description("호수 자유 배치물")] lakeFree
+}
+
+public enum CostumeFilter 
+{
+    [Description("모든 아이템")] all,
+    [Description("머리 장식")] head,
+    [Description("한벌옷")] body
+}
+
+public enum FishingFilter
+{
+    [Description("모든 아이템")] all,
+    [Description("낚싯대")] pole,
+    [Description("낚시찌")] bait,
+    [Description("미끼")] bobber
+}
--- Assets/0.Scripts/Store/StoreUI/SortDropdown.cs
+++ Assets/0.Scripts/Store/StoreUI/SortDropdown.cs
@@ -23,7 +23,7 @@ public class SortDropdown : StoreDropdownBase
     void Start()
     {
         // 드롭다운 옵션 리스트 전달
-        GetOptionList<StoreSort>(Enum.GetValues(typeof(StoreSort)));
+        optionList = StoreManager.Instance.GetEnumList<StoreSort>(Enum.GetValues(typeof(StoreSort)));
 
         SetOptions();
     }
