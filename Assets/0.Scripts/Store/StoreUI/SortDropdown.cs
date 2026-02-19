2bca45b68ac41271f32066e82aec88ce733dbce4
jeonjyu <jihoojeon77@gmail.com>
2026-02-19 12:26:37 +0900

    [feat] 드롭다운 옵션 불러오는 기능

---
+++ Assets/0.Scripts/Store/StoreUI/SortDropdown.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using System.ComponentModel;
+
+
+public enum StoreSort
+{
+    [Description("보유")]
+    gain = 1,
+    [Description("미보유")]
+    unGain,
+    [Description("가격 높은 순")]
+    highToLow,
+    [Description("최근 구매 순")]
+    recentPurchase,
+    [Description("이름 순")]
+    name
+}
+
+public class SortDropdown : StoreDropdownBase
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // 드롭다운 옵션 리스트 전달
+        GetOptionList<StoreSort>(Enum.GetValues(typeof(StoreSort)));
+
+        SetOptions();
+    }
+}
---
+++ Assets/0.Scripts/Store/StoreUI/StoreDropdownBase.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using TMPro;
+using UnityEngine;
+
+public class StoreDropdownBase : MonoBehaviour
+{
+    TMP_Dropdown dropdown;
+
+    List<string> optionList = new List<string>();
+
+    void Awake()
+    {
+        dropdown = GetComponent<TMP_Dropdown>();
+    }
+
+    public void SetOptions()
+    {
+        dropdown.ClearOptions();
+
+        foreach (var option in optionList)
+        {
+            dropdown.options.Add(new TMP_Dropdown.OptionData(option));
+        }
+    }
+
+
+    public void GetOptionList<T>(Array optionEnum) where T : Enum
+    {
+        //Enum[] optionArr = (Enum[])Enum.GetValues(typeof(T));
+        List<string> lst = new List<string>(optionEnum.Length);
+
+        foreach(T option in optionEnum)
+            lst.Add(GetEnumDesc(option));
+
+        optionList = new List<string>(lst);
+    }
+
+    // 각 enum에 지정된 description 반환
+    public string GetEnumDesc<T>(T value) where T : Enum 
+    {
+        FieldInfo fieldInfo = value.GetType().GetField(value.ToString());
+        DescriptionAttribute description = fieldInfo.GetCustomAttribute(typeof(DescriptionAttribute), false) as DescriptionAttribute;
+
+        return description.Description;
+    }
+}
