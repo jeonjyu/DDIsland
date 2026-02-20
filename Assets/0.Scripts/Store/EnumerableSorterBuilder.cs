b92dc9237d1c46b246ba65939164e4efcccf5867
jeonjyu <jihoojeon77@gmail.com>
2026-02-20 23:28:23 +0900

    [feat] 정렬시 다중 조건을 적용하기 위한 체이닝 빌더 추가

---
+++ Assets/0.Scripts/Store/EnumerableSorterBuilder.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// 정렬 기준 체이닝하는 클래스
+public static class EnumerableSorterBuilder
+{
+    // 오름차순
+    public static IOrderedEnumerable<T> AppendOrderBy<T, TKey>(this IEnumerable<T> source, Func<T, TKey> func)
+    {
+        // 이전에 정렬된 건지 확인
+        if (source is IOrderedEnumerable<T>)
+            return ((IOrderedEnumerable<T>)source).ThenBy(func);
+        else
+            return source.OrderBy(func);
+    }
+
+    // 내림차순
+    public static IOrderedEnumerable<T> AppendOrderByDesending<T, TKey>(this IEnumerable<T> source, Func<T, TKey> func)
+    {
+        // 이전에 정렬된 건지 확인
+        if (source is IOrderedEnumerable<T>)
+            return ((IOrderedEnumerable<T>)source).ThenByDescending(func);
+        else
+            return source.OrderByDescending(func);
+    }
+}
