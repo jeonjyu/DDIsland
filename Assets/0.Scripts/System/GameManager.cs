23cc879007ec2efb945b08e2d1f116573e66f601
jeonjyu <jihoojeon77@gmail.com>
2026-02-25 23:06:40 +0900

    [feat] 게임 매니저에 플레이어 재화 멤버 추가

--- Assets/0.Scripts/System/GameManager.cs
+++ Assets/0.Scripts/System/GameManager.cs
@@ -2,8 +2,19 @@ using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
+    int playerGold = 0;
+    public int PlayerGold => playerGold;
+
     protected override void Awake()
     {
         base.Awake();
+    }
+
+    public void SetGold(int gold)
+    {
+        if (gold < 0)
+            playerGold += playerGold > gold ? gold : 0;
+        else
+            playerGold += gold;
     }
 }
