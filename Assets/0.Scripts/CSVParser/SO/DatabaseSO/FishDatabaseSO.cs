fc1199ede7fc8ffc26721f0f122fca0fa9b63b7a
Hyun <shs217shs@naver.com>
2026-02-26 12:43:29 +0900

    [Remove] 테스트용 파일 제거

--- Assets/0.Scripts/CSVParser/SO/DatabaseSO/FishDatabaseSO.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections.Generic;
-
-[CreateAssetMenu(fileName = "FishDatabaseSO", menuName = "Scriptable Objects/DataBase/FishDatabaseSO")]
-public class FishDatabaseSO : TableDatabase<int, FishDataSO>
-{
-
-}
--- Assets/0.Scripts/Data/UpgradeData.cs
+++ Assets/0.Scripts/Data/UpgradeData.cs
@@ -2,8 +2,8 @@ using System;
 using UnityEngine;
 
 
-//public enum ApplyType { None = 0, Add=1, Set=2 }
-//public enum StatType { None =0, BaseHunger=1, BaseStamina=2, BaseMoveSpeed=3, BaseFishingSpeed=4 }
+public enum ApplyType { None = 0, Add=1, Set=2 }
+public enum StatType { None =0, BaseHunger=1, BaseStamina=2, BaseMoveSpeed=3, BaseFishingSpeed=4 }
 
 
 [System.Serializable]
