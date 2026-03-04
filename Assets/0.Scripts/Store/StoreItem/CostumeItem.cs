24c6ccde619739bd65b81eb9679ec9787ca398be
jeonjyu <jihoojeon77@gmail.com>
2026-03-04 22:48:12 +0900

    [remove] 미사용 파일 삭제

--- Assets/0.Scripts/Store/StoreItem/CostumeItem.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-
-public class CostumeItem : StoreItemBase
-{
-    CostumeType _costumeType;
-}
--- Assets/0.Scripts/Store/StoreTest/StoreCostumeItemSO.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "StoreCostumeItemSO", menuName = "Scriptable Objects/StoreCostumeItemSO")]
-public class StoreCostumeItemSO : StoreItemBaseSO<CostumeFilter>
-{
-    public StoreCat store => StoreCat.costume;
-}
