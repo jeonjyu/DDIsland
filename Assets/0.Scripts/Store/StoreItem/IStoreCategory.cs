f5d25820fdcf6745743a396b0491739d60085ff9
jeonjyu <jihoojeon77@gmail.com>
2026-03-04 22:51:20 +0900

    [remove] 미사용 파일 삭제

--- Assets/0.Scripts/Store/StoreItem/IStoreCategory.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IStoreCategory<T> where T : Enum
-{
-    void AddToCategory(StoreItemBaseSO<T> item);
-    void RemoveFromCategory(StoreItemBaseSO<T> item);
-}
