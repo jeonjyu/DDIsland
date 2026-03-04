852bb258d164f9c53471ee9cba331a6015cbc6ad
jeonjyu <jihoojeon77@gmail.com>
2026-03-04 23:00:05 +0900

    [remove] 데이터 구조 변경 후 미사용 파일 삭제

--- Assets/0.Scripts/Store/StoreTest/StoreInteriorItemSO.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "StoreInteriorItemSO", menuName = "Scriptable Objects/StoreInteriorItemSO")]
-public class StoreInteriorItemSO : StoreItemBaseSO<InteriorFilter> 
-{
-    public StoreCat store => StoreCat.interior;
-}
--- Assets/0.Scripts/Store/StoreTest/StoreItemBaseSO.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-public class StoreItemBaseSO<T> : StoreItem where T : System.Enum
-{
-    public T filter;
-
-    public override Enum GetFilter() => filter;
-}
