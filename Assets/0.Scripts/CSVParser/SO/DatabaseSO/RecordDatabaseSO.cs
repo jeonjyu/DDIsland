6baf44912cb47948d7a20e03b9be0a7cb1e8113d
Hyun <shs217shs@naver.com>
2026-03-13 22:34:51 +0900

    [Refactor] 생명주기 관련 리팩토링

---
+++ Assets/0.Scripts/CSVParser/SO/DatabaseSO/RecordDatabaseSO.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "RecordDatabaseSO", menuName = "Scriptable Objects/DataBase/RecordDatabaseSO")]
+public class RecordDatabaseSO : TableDatabase<int, RecordDataSO>
+{
+
+}
