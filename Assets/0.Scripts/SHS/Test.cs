66fb17f5e7c28a941eade4d9912c5f6e847f300f
Complex-Answer <zstep0101@gmail.com>
2026-03-18 11:50:51 +0900

    Feat: 데이터 초기화 버튼 추가
    데이터 초기화가 가능한 버튼을 추가했습니다

--- Assets/0.Scripts/SHS/Test.cs
+++ Assets/0.Scripts/SHS/Test.cs
@@ -7,4 +7,34 @@ public class Test : MonoBehaviour
         Application.Quit();
     }
 
+    public void OnClick_ClearPlayerPrefsData()
+    {
+        PlayerPrefs.DeleteAll();
+
+        PlayerPrefs.Save();
+    }
+    public void OnClick_ClearFirebaseData()
+    {
+        if (DataManager.Instance == null || DataManager.Instance.Hub == null)
+        {
+            Debug.LogError("DataManager 또는 Hub가 없습니다!");
+            return;
+        }
+
+        DataManager.Instance.Hub._allUserData = new UserAllData();
+
+        DataManager.Instance.Hub.SaveAllData();
+
+        if (ItemManager.Instance != null)
+        {
+            ItemManager.Instance.playerItemDatas.Clear();
+        }
+
+        var buildingMgr = FindFirstObjectByType<BuildingManager>();
+        if (buildingMgr != null)
+        {
+            buildingMgr.ClearAll();
+        }
+    }
+
 }
