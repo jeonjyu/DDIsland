5fe8c212fca0e6241b3b813353ae2734345fdec2
jeonjyu <jihoojeon77@gmail.com>
2026-03-24 15:03:57 +0900

    [chore] 호수, 섬 인테리어 데이터 테이블 갱신

--- Assets/0.Scripts/CSVParser/SO/DataSO/LakeStoreDataSO.cs
+++ Assets/0.Scripts/CSVParser/SO/DataSO/LakeStoreDataSO.cs
@@ -20,14 +20,8 @@ public class LakeStoreDataSO : TableBase<int>
     // 인테리어id
     [field: SerializeField] public int InteriorId { get; private set; }
 
-    // 테마 그룹
-    [field: SerializeField] public int LakeInteriorItemGroup { get; private set; }
-
     // 테마 이름
     [field: SerializeField] public string LakeThemeName { get; private set; }
-
-    // 타입
-    [field: SerializeField] public LakeStore_ItemType lakestore_itemType { get; private set; }
 
     // 최대 보유 가능 갯수
     [field: SerializeField] public int MaxCount { get; private set; }
@@ -43,9 +37,6 @@ public class LakeStoreDataSO : TableBase<int>
 
     // UI 미니 이미지 리소스
     [field: SerializeField] public Sprite InteriorImgPath_Sprite { get; private set; }
-
-    // 테마 리소스
-    [field: SerializeField] public Sprite InteriorThemeImgPath_Sprite { get; private set; }
 
     // 부모 클래스의 ID 반환 추상 메서드
     public override int GetID() => IDLakeStore;
