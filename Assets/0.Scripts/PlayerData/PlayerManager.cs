b39d20c70d3e076c388e527e4c9a14d6c6e42503
jeonjyu <jihoojeon77@gmail.com>
2026-03-24 12:11:09 +0900

    [refactor] 장착 코스튬 아이디 필드, 프로퍼티 변경
    필드에 접근하는 방식에서 딕셔너리 값에 접근하도록 변경

--- Assets/0.Scripts/PlayerData/PlayerManager.cs
+++ Assets/0.Scripts/PlayerData/PlayerManager.cs
@@ -11,29 +11,30 @@ public class PlayerManager : Singleton<PlayerManager>
     [Header("씬에 배치된 플레이어 오브젝트")]
     [SerializeField] GameObject _player;
 
+    // 코스튬 장착에서 필요하시면 사용
     [Header("플레이어의 코스튬 장착 소켓")]
     [SerializeField] Transform _SocketCostumeHat;
     [SerializeField] Transform _SocketCostumeTie;
     
 
-    int _currentHatID = 0;
-    int _currentTieID = 0;
-    int _currentPoleID = 0;
-    int _currentBaitID = 0;
-    int _currentBobberID = 0;
+    //int _currentHatID = 0;
+    //int _currentTieID = 0;
+    //int _currentPoleID = 0;
+    //int _currentBaitID = 0;
+    //int _currentBobberID = 0;
 
     // 플레이어가 장비중인 아이템 딕셔너리
     // 코스튬(모자, 한벌옷), 낚시 아이템 (미끼, 낚시대, 낚시찌)
     Dictionary<string, int> playerEquip = new Dictionary<string, int>();
 
-#endregion
+    #endregion
 
-#region property
-    public int CurrentHatID {get => _currentHatID; private set { _currentHatID = value; } }
-    public int CurrentTieID {get => _currentTieID; private set { _currentTieID = value; } }
-    public int CurrentPoleID { get => _currentPoleID; private set { _currentPoleID = value; } }
-    public int CurrentBaitID { get => _currentBaitID; set => _currentBaitID = value; }
-    public int CurrentBobberID { get => _currentBobberID; set => _currentBobberID = value; }
+    #region property
+    public int CurrentHatID { get => playerEquip[CostumeType.Head.ToString()]; private set { playerEquip[CostumeType.Head.ToString()] = value; } }
+    public int CurrentTieID { get => playerEquip[CostumeType.Body.ToString()]; private set { playerEquip[CostumeType.Body.ToString()] = value; } }
+    public int CurrentPoleID { get => playerEquip[FishingItemType.Pole.ToString()]; private set { playerEquip[FishingItemType.Pole.ToString()] = value; } }
+    public int CurrentBaitID { get => playerEquip[FishingItemType.Bait.ToString()]; set => playerEquip[FishingItemType.Bait.ToString()] = value; }
+    public int CurrentBobberID { get => playerEquip[FishingItemType.Bobber.ToString()]; set => playerEquip[FishingItemType.Bobber.ToString()] = value; }
 
     public Dictionary<string, int> PlayerEquip = new Dictionary<string, int>();
 
@@ -47,22 +48,22 @@ public class PlayerManager : Singleton<PlayerManager>
         if(_SocketCostumeTie == null) _SocketCostumeTie = _player.transform.Find("Socket_CoustumeTie");
 
         playerEquip.Clear();
-        playerEquip.Add(CostumeType.Head.ToString(), _currentHatID);
-        playerEquip.Add(CostumeType.Body.ToString(), _currentTieID);
-        playerEquip.Add(FishingItemType.Pole.ToString(), _currentPoleID);
-        playerEquip.Add(FishingItemType.Bait.ToString(), _currentBaitID);
-        playerEquip.Add(FishingItemType.Bobber.ToString(), _currentBobberID);
+        playerEquip.Add(CostumeType.Head.ToString(), 0);
+        playerEquip.Add(CostumeType.Body.ToString(), 0);
+        playerEquip.Add(FishingItemType.Pole.ToString(), 0);
+        playerEquip.Add(FishingItemType.Bait.ToString(), 0);
+        playerEquip.Add(FishingItemType.Bobber.ToString(), 0);
     }
 
     // 현재 활성화상태인 코스튬 아이템의 아이디를 받아옴
-    // todo : 플레이어 정보에 저장된 코스튬 아이디를 받아와 저장하도록 수정
     void Start()
     {
-        playerEquip[CostumeType.Head.ToString()] = 0;
-        playerEquip[CostumeType.Body.ToString()] = 0;
-        playerEquip[FishingItemType.Pole.ToString()] = 0;
-        playerEquip[FishingItemType.Bait.ToString()] = 0;
-        playerEquip[FishingItemType.Bobber.ToString()] = 0;
+        // todo : 플레이어 정보에 저장된 코스튬 아이디를 받아와 저장하도록 수정
+        //playerEquip[CostumeType.Head.ToString()] = 0;
+        //playerEquip[CostumeType.Body.ToString()] = 0;
+        //playerEquip[FishingItemType.Pole.ToString()] = 0;
+        //playerEquip[FishingItemType.Bait.ToString()] = 0;
+        //playerEquip[FishingItemType.Bobber.ToString()] = 0;
     }
 
     // 코스튬 ID 변경
@@ -70,12 +71,12 @@ public class PlayerManager : Singleton<PlayerManager>
     {
         if(CompareID(item))
         {
-            Debug.Log("장착 아이템 " + playerEquip[item.Filter.ToString()] + "에서 0으로 변경");
+            Debug.Log($"장착 아이템({item.Filter}) " + playerEquip[item.Filter.ToString()] + "에서 0으로 변경");
             playerEquip[item.Filter.ToString()] = 0;
         }
         else
         {
-            string logTxt = "장착 아이템 " + playerEquip[item.Filter.ToString()];
+            string logTxt = $"장착 아이템({item.Filter}) " + playerEquip[item.Filter.ToString()];
         
             playerEquip[item.Filter.ToString()] = item.ObjectId;
 
