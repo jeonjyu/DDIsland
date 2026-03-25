53590849e5bca2332b95df0880bfe9dfb21e631e
Complex-Answer <zstep0101@gmail.com>
2026-03-25 17:05:07 +0900

    Feat: 우편함 정상 기능 확인 및 인게임씬 병합
    우편 기능이 정상적으로 작동이 되는 것을 확인했습니다
    현재는 테스트 코드로 우편함을 작동하고 있기 때문에 우편함 증식이 일어나고 있습니다
    추후 서버와 연동하여 공지를 올리는 방식도 작동 시킬 수 있습니다

---
+++ Assets/0.Scripts/UI/Mail/UI_MailPopup.cs
@@ -0,0 +1,75 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_MailPopup : MonoBehaviour
+{
+    [Header("UI 연결")]
+    [SerializeField] private TMP_Text _titleText;
+    [SerializeField] private TMP_Text _contentText;
+    [SerializeField] private Image _rewardImage;
+    [SerializeField] private TMP_Text _rewardCountText;
+
+    [Header("UI 버튼 연결")]
+    [SerializeField] private Button _actionButton;         // 하단 중앙 버튼 (받기 or 닫기)
+    [SerializeField] private TMP_Text _actionButtonText;
+
+    private MailData _currentData;
+
+
+    public void OpenPopup(MailData mail)
+    {
+        _currentData = mail;
+        gameObject.SetActive(true);
+
+        _titleText.text = _currentData._title;
+        _contentText.text = _currentData._content;
+
+        MailManager.Instance.MarkAsRead(_currentData._mailID);
+
+        if (_currentData._rewardItemID > 0)
+        {
+            _rewardImage.gameObject.SetActive(true);
+            _rewardCountText.text = $"{_currentData._rewardCount}";
+            _rewardImage.sprite = DataManager.Instance.CurrencyDatabase.CurrencyInfoData[_currentData._rewardItemID].CurrencyImgPath_Sprite;
+        }
+        else
+        {
+            _rewardImage.gameObject.SetActive(false);
+            _rewardCountText.text = "";
+        }
+
+        UpdateButtonState();
+    }
+
+    private void UpdateButtonState()
+    {
+        _actionButton.onClick.RemoveAllListeners();
+
+        bool isClaimed = MailManager.Instance.IsMailClaimed(_currentData._mailID);
+
+        // 보상이 있고, 아직 수령하지 않았다면 -> '받기'
+        if (_currentData._rewardItemID > 0 && !isClaimed)
+        {
+            _actionButtonText.text = "받기";
+            _actionButton.onClick.AddListener(OnClickClaim);
+        }
+        // 보상이 없거나 이미 수령했다면 -> '닫기'
+        else
+        {
+            _actionButtonText.text = "닫기";
+            _actionButton.onClick.AddListener(ClosePopup);
+        }
+    }
+
+    private void OnClickClaim()
+    {
+        MailManager.Instance.ClaimReward(_currentData);
+        UpdateButtonState();
+    }
+
+    public void ClosePopup()
+    {
+        gameObject.SetActive(false);
+    }
+}
